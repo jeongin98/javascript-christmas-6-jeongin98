8b01639f3e43675ccc01f8bcaf391a8eb4b132cb
jeongin98 <eon7500@gmail.com>
2023-11-14 00:44:18 +0900

    Test(Validation): 주문 정보를 정제한 뒤 반환하는 함수 테스트 구현

---
+++ __tests__/ValidationTest.js
@@ -0,0 +1,18 @@
+import Validation from '../utils/validation.js';
+
+describe('Validation 테스트', () => {
+  test('입력받은 주문 정보를 정제한 뒤 반환', async () => {
+    // given
+    const input = '타파스-1,제로콜라-1';
+    const passResult = [
+      { category: 'Appetizers', menuName: '타파스', orderQuantity: 1 },
+      { category: 'Drinks', menuName: '제로콜라', orderQuantity: 1 },
+    ];
+
+    // when
+    const result = Validation.changeIntoOrderInfo(input);
+
+    // then
+    expect(result).toEqual(passResult);
+  });
+});
