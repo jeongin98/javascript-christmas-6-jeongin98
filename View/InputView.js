163a9192ef70cfb31ef04cb5f531339a8bdd6dd3
jeongin98 <eon7500@gmail.com>
2023-11-13 21:48:14 +0900

    Feat(Input): 날짜 입력받기

--- View/InputView.js
+++ View/InputView.js
@@ -1,9 +1,30 @@
+import { Console } from '@woowacourse/mission-utils';
+import { INPUT_MESSAGE, ERROR_MESSAGE } from '../utils/messages.js';
+// import Validation from '../utils/validation.js';
+
 const InputView = {
   async readDate() {
-    const input = await Console.readLineAsync('12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)');
-    // ...
+    const isValidDate = false;
+    let date;
+    while (!isValidDate) {
+      try {
+        const userDateInput = await Console.readLineAsync(INPUT_MESSAGE.readDate);
+        date = Number(userDateInput);
+        // Validation. // throw Error
+        isValidDate = true;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+    return date;
   },
-  // ...
+
+  async readOrder() {
+    const order = await Console.readLineAsync(INPUT_MESSAGE.readOrder);
+    // 유효성
+    // 재검사
+    return order;
+  },
 };
 
 export default InputView;
